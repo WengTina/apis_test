8dab032433c174a30794911a7450a18a5b6506e8
WengTina <tina.weng8@gmail.com>
2023-06-04 03:59:16 +0800

    feat: presenter

--- internal/v1/presenter/roles/roles_presenter.go
+++ internal/v1/presenter/roles/roles_presenter.go
@@ -5,6 +5,7 @@ import (
 
 	"eirc.app/internal/pkg/code"
 	"eirc.app/internal/pkg/log"
+	"eirc.app/internal/pkg/util"
 	preset "eirc.app/internal/v1/presenter"
 	"eirc.app/internal/v1/structure/roles"
 	"github.com/gin-gonic/gin"
@@ -27,9 +28,9 @@ import (
 func (p *presenter) Created(ctx *gin.Context) {
 	// Todo 將UUID改成登入的使用者
 	trx := ctx.MustGet("db_trx").(*gorm.DB)
-
+	createBy := util.GenerateUUID()
 	input := &roles.Created{}
-
+	input.CreatedBy = createBy
 	if err := ctx.ShouldBindJSON(input); err != nil {
 		log.Error(err)
 		ctx.JSON(http.StatusOK, code.GetCodeMessage(code.FormatError, err.Error()))
